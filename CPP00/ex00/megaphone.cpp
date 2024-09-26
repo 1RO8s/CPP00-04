1f1c3a008f8b9da6c8a110e3efa13e40fa4caeb8
1RO8s <saki0220hiro@gmail.com>
2024-09-26 19:57:34 +0900

    CPP00/ex00の実装

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/20 01:54:40 by hnagasak          #+#    #+#             */
+/*   Updated: 2024/09/26 15:49:57 by hnagasak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <locale>
+
+std::string to_upper(std::string src) {
+    std::string result;
+    for (std::size_t i = 0; i < src.size(); i++) {
+        result += std::toupper(src[i]);
+    }
+    return result;
+}
+
+void putstr(std::string str) {
+    std::cout << str;
+}
+
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        putstr("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+        std::cout << std::endl;
+        return 0;
+    }
+    for (int i = 1; i < argc; i++) {
+        putstr(to_upper(argv[i]));
+    }
+    std::cout << std::endl;
+    return 0;
+}
