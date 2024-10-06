36a235410f8d38ccc3b373c0eada18165a18c67a
1RO8s <saki0220hiro@gmail.com>
2024-10-06 11:35:33 +0900

    [CPP01/ex05] 出力する文字列の修正

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -19,19 +19,19 @@ Harl::~Harl() {
 }
 
 void Harl::debug(void) {
-    std::cout << "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+  std::cout << "[ DEBUG ]:\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info(void) {
-    std::cout << "INFO: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger!" << std::endl;
+  std::cout << "[ INFO ]:\nI cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger!" << std::endl;
 }
 
 void Harl::warning(void) {
-    std::cout << "WARNING: I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+  std::cout << "[ WARNING ]:\nI think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(void) {
-    std::cout << "ERROR: This is unacceptable! I want to speak to the manager now." << std::endl;
+  std::cout << "[ ERROR ]:\nThis is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void Harl::invalid(void) {
