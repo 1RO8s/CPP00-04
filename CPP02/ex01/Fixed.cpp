97e9b1ca66099befdb938764992f354e170138cf
1RO8s <saki0220hiro@gmail.com>
2024-10-11 02:09:04 +0900

    [CPP02/ex01] 実装

---
+++ CPP02/ex01/Fixed.cpp
@@ -0,0 +1,55 @@
+#include "Fixed.hpp"
+
+#include <cmath>
+
+Fixed::Fixed() : _rawBits(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& src) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = src;
+}
+
+Fixed::Fixed(const int num) {
+    std::cout << "Int constructor called" << std::endl;
+    this->_rawBits = num << Fixed::FRACTIONAL_BITS;
+}
+
+Fixed::Fixed(const float num) {
+    std::cout << "Float constructor called" << std::endl;
+    this->_rawBits = roundf(num * (1 << Fixed::FRACTIONAL_BITS));
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other) {
+        this->_rawBits = other.getRawBits();
+    }
+    return *this;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits() const {
+    return this->_rawBits;
+}
+
+void Fixed::setRawBits(int const raw) {
+    this->_rawBits = raw;
+}
+
+std::ostream& operator<<(std::ostream& ostream, const Fixed& fixed) {
+    ostream << fixed.toFloat();
+    return ostream;
+}
+
+float Fixed::toFloat() const {
+    return (float)this->_rawBits / (1 << Fixed::FRACTIONAL_BITS);
+}
+
+int Fixed::toInt() const {
+    return this->_rawBits >> Fixed::FRACTIONAL_BITS;
+}
---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+  public:
+    Fixed();
+    Fixed(const Fixed& src);
+    Fixed(const int num);
+    Fixed(const float num);
+    Fixed& operator=(const Fixed& other);
+    ~Fixed();
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+    float toFloat(void) const;
+    int toInt(void) const;
+
+  private:
+    int _rawBits;
+    static const int FRACTIONAL_BITS = 8;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
+
+#endif
