173d1a8f5bb30cea6d16b47b7b6a87c25dafb702
1RO8s <saki0220hiro@gmail.com>
2024-10-14 08:22:14 +0900

    [CPP03/ex00] テストコードの修正など

--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -2,10 +2,11 @@
 #include <cassert>
 
 void info(const std::string &str) {
-    std::cout << "\033[3;37m" << str << "\033[0m" << std::endl;
+    std::cout << "\033[3;36m" << str << "\033[0m" << std::endl;
 }
 
 void testClapTrap() {
+    info("###### Test: ClapTrap ######");
     // コンストラクタのテスト
     info("--- Test: Constructor ---");
     ClapTrap defaultClapTrap;
@@ -13,7 +14,6 @@ void testClapTrap() {
     
     // 名前のゲッターとセッターのテスト
     info("--- Test: getName, setName ---");
-    std::string name = "";
     assert(namedClapTrap.getName() == "namedClapTrap");
     namedClapTrap.setName("newNamedClapTrap");
     assert(namedClapTrap.getName() == "newNamedClapTrap");
@@ -46,10 +46,33 @@ void testClapTrap() {
     info("--- Test: beRepaired ---");
     namedClapTrap.beRepaired(20);
     assert(namedClapTrap.getHitPoints() == 90);
+
+    //　コピー今ストラク他のテスト
+    info("--- Test: Copy Constructor ---");
+    ClapTrap copiedClapTrap(namedClapTrap);
+    assert(copiedClapTrap.getName() == "newNamedClapTrap");
+    assert(copiedClapTrap.getHitPoints() == namedClapTrap.getHitPoints());
+    assert(copiedClapTrap.getEnergyPoints() == namedClapTrap.getEnergyPoints());
+    assert(copiedClapTrap.getAttackDamage() == namedClapTrap.getAttackDamage());
+
+    // コピー代入演算子のテスト
+    info("--- Test: Copy Assignment Operator ---");
+    ClapTrap assignedClapTrap;
+    assignedClapTrap = namedClapTrap;
+    assert(assignedClapTrap.getName() == "newNamedClapTrap");
+    assert(assignedClapTrap.getHitPoints() == namedClapTrap.getHitPoints());
+    assert(assignedClapTrap.getEnergyPoints() == namedClapTrap.getEnergyPoints());
+    assert(assignedClapTrap.getAttackDamage() == namedClapTrap.getAttackDamage());
+
+    info("--- Test: Destructor ---");
+    assignedClapTrap.setName("assignedClapTrap");
+    copiedClapTrap.setName("copiedClapTrap");
+    namedClapTrap.setName("namedClapTrap");
+    defaultClapTrap.setName("defaultClapTrap");
 }
 
 int main() {
     testClapTrap();
-    std::cout << "All tests passed!" << std::endl;
+    std::cout << "\033[3;32m----- All tests passed! -----\033[0m" << std::endl;
     return 0;
 }
